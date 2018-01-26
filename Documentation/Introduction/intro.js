8eb1e8824c6a577a1b6c02bb62fccb3ba0bbf9e6
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-25 23:33:45 -0800

    Introduction of ExpressJS

---
+++ Documentation/Introduction/intro.js
@@ -0,0 +1,38 @@
+/**
+ * --------
+ * Express
+ * --------
+ * ExpressJS is a web application framework that provides you with a simple API to build websites, 
+ * web apps and back ends. With ExpressJS, you need not worry about low level protocols, processes, etc
+ * 
+ * Express provides a minimal interface to build our applications. It provides us the tools that are 
+ * required to build our app. It is flexible as there are numerous modules available on npm, which can 
+ * be directly plugged into Express.
+ * 
+ * Express was developed by TJ Holowaychuk and is maintained by the Node.js foundation and numerous open 
+ * source contributors.
+ * 
+ * --------------
+ * Why Express ?
+ * --------------
+ * Unlike its competitors like Rails and Django, which have an opinionated way of building applications, 
+ * Express has no "best way" to do something. It is very flexible and pluggable.
+ * 
+ * ----
+ * Pug
+ * ----
+ * Pug (earlier known as Jade) is a terse language for writing HTML templates. It −
+ *      Produces HTML
+ *      Supports dynamic code
+ *      Supports reusability (DRY)
+ * 
+ * It is one of the most popular template language used with Express.
+ * 
+ * ---------------------
+ * MongoDB and Mongoose
+ * ---------------------
+ * MongoDB is an open-source, document database designed for ease of development and scaling. This 
+ * database is also used to store data.
+ * Mongoose is a client API for node.js which makes it easy to access our database from our Express 
+ * application.
+ */
