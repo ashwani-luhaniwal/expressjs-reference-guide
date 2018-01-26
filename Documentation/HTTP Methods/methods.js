a0f009c9abe9af455236ed1aac49e36b5adef0dd
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-26 11:38:58 -0800

    HTTP Methods in ExpressJS

---
+++ Documentation/HTTP Methods/methods.js
@@ -0,0 +1,14 @@
+/**
+ * -------------------------
+ * ExpressJS - HTTP Methods
+ * -------------------------
+ * The HTTP method is supplied in the request and specifies the operation that the client has requested.
+ *  - GET: The GET method requests a representation of the specified resource. Requests using GET should 
+ *         only retrieve data and should have no other effect.
+ *  - POST: The POST method requests that the server accept the data enclosed in the request as a new 
+ *          object/entity of the resource identified by the URI.
+ *  - PUT: The PUT method requests that the server accept the data enclosed in the request as a 
+ *         modification to existing object identified by the URI. If it does not exist then the PUT 
+ *         method should create one.
+ *  - DELETE: The DELETE method requests that the server delete the specified resource.
+ */
