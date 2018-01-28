17060969a5a0970974497b37e4c435fb119dc51b
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-28 10:55:33 -0800

    Scaffolding in Express

---
+++ Documentation/Scaffolding/app.js
@@ -0,0 +1,35 @@
+/**
+ * ------------------------
+ * ExpressJS - Scaffolding
+ * ------------------------
+ * Scaffolding allows us to easily create a skeleton for a web application. We manually create our 
+ * public directory, add middleware, create separate route files, etc. A scaffolding tool sets up all 
+ * these things for us so that we can directly get started with building our application.
+ * 
+ * The scaffolder we will use is called Yeoman. It is a scaffolding tool built for Node.js but also has 
+ * generators for several other frameworks (like flask, rails, django, etc.). To install Yeoman, enter 
+ * the following command in your terminal −
+ * 
+ *      npm install -g yeoman
+ * 
+ * Yeoman uses generators to scaffold out applications. In this tutorial, we will use the 
+ * 'generator-Express-simple'. To install this generator, enter the following command in your terminal −
+ *  
+ *      npm install -g generator-express-simple
+ * 
+ * To use this generator, enter the following command −
+ * 
+ *      yo express-simple test-app
+ * 
+ * You will be asked a few simple questions like what things you want to use with your app. Select the 
+ * following answers, or if you already know about these technologies then go about choosing how you 
+ * want them to be.
+ * 
+ * It will then create a new application for you, install all the dependencies, add few pages to your 
+ * application(home page, 404 not found page, etc.) and give you a directory structure to work on.
+ * 
+ * This generator creates a very simple structure for us. Explore the many generators available 
+ * for Express and choose the one that fits you right. Steps to working with all generators is the 
+ * same. You will need to install a generator, run it using Yeoman; it will ask you some questions 
+ * and then create a skeleton for your application based on your answers.
+ */
